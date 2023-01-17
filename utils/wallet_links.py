c502b6baa3b0cebd3987e05890f4377aab1d9739
amirmistik <57581726+amirmistikad@users.noreply.github.com>
2023-01-17 16:14:38 +0500

    Fix bags

--- utils/wallet_links.py
+++ utils/wallet_links.py
@@ -4,4 +4,5 @@ def send_ton(user_id):
     return f"ton://transfer/{WALLET}?text={user_id}"
 
 def send_lave(user_id):
-    return f"ton://transfer/{WALLET}?jetton={LAVE}&text={user_id}"
+    # hard coded lave address, cuz it has tonkeeper bag
+    return f"ton://transfer/{WALLET}?jetton=EQBl3gg6AAdjgjO2ZoNU5Q5EzUIl8XMNZrix8Z5dJmkHUfxI&text={user_id}"
